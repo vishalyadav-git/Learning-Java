155dbb8ffa202d6db27f5ebf60afde697599c255
Vishal Yadav <yadavvishal00101@gmail.com>
2025-02-10 18:09:15 +0000

    Sum of integer

---
+++ 01_basics/sumOfNum.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class sumOfNum{
+    public static void main(String args[]){
+        Scanner a = new Scanner(System.in);
+        int num1 = a.nextInt();
+        int num2 = a.nextInt();
+        int sum = num1 + num2 ;
+        System.out.println(sum) ;
+    }
+}
