f743053bf1e98626934d2de85a0a653f6c31ee1a
Vishal Yadav <yadavvishal00101@gmail.com>
2025-02-10 17:48:11 +0000

    Java Basics

---
+++ JavaBasic.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class JavaBasic {
+
+    public static void main(String[] args) {
+        /*
+        System.out.println("Hello Vishal!");//line space using ln
+        System.out.println("Hello Vishal!");//line space using ln
+        System.out.println("Hello Vishal!");//line space using ln
+         */
+
+        Scanner s = new Scanner(System.in);
+        String input = s.nextLine();
+        System.out.println(input);
+    }
+}
+
+/*
+Primitive Datatypes in java
+int  4 bytes
+float  4 bytes
+char  2 bytes
+byte  1 bytes
+short   2 bytes
+long   8 bytes
+double   8 bytes
+boolen   1 bytes
+*/
+
+/*
+ To Print 
+ System.out.println("Statement")
+
+
+ Input
+ 1. import java.util.*;
+ 2. make a class
+ 3. Scanner class  object instance
+ 4.
+ */
