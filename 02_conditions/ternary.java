ce2bd2697e6e3980ea9e4fa232da92b3db566c5a
Vishal Yadav <yadavvishal00101@gmail.com>
2025-02-24 12:42:45 +0000

    while loop

---
+++ 02_conditions/ternary.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class ternary {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int marks = sc.nextInt();
+        String result = marks>=32 ? "pass" : "fail";
+        System.out.println(result);
+    }
+}
---
+++ 02_conditions/whilee.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class whilee{
+    public static void main(String args[]){
+        int i = 0;
+        while(i<=99){
+            System.out.println("Hello world");
+            i++;
+        }
+    }
+}
