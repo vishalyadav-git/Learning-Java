346551a51e93dbe98b82add0479e52b16456271d
Vishal Yadav <yadavvishal00101@gmail.com>
2025-02-25 15:13:12 +0000

    multiplication of number

---
+++ 01_basics/multiplicationTable.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class multiplicationTable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int product = 0;
+        System.out.println("enter the no: ");
+        for(int i = 1; i<=10; i++){
+            product = n*i;
+            System.out.println("Multiplication is: " +product);
+        
+        }
+    }
+}
