d80ad349fb5cbff59bdcbff93ae5a6bd0bcfda3b
Vishal Yadav <yadavvishal00101@gmail.com>
2025-03-07 13:46:38 +0000

    Number pyramid

---
+++ 05_pattern/numPyramid.java
@@ -0,0 +1,19 @@
+public class numPyramid {
+    public static void pattern(int n){
+        for(int i=1; i<=n; i++){
+            // spaces -(n-i)
+            for(int j=1; j<=(n-i); j++){
+                System.out.print(" ");
+            }
+            // number
+            for(int j=1; j<=i; j++){
+               System.out.print(i +" ");
+            }
+            
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        pattern(5);
+    }
+}
