923ea354c0f40f17b49921cd014b91ab19d70232
Vishal Yadav <yadavvishal00101@gmail.com>
2025-03-07 06:19:01 +0000

    Floyd Pyramid

---
+++ 04_pattern/floydPyramid.java
@@ -0,0 +1,15 @@
+public class floydPyramid {
+    public static void pattern(int n){
+        int counter = 1;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(counter +" ");
+                counter++;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        pattern(5);
+    }
+}
