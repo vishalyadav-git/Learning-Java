c12d44083a48cdf4738c49fe340cf2dba14b2a9a
Vishal Yadav <yadavvishal00101@gmail.com>
2025-03-07 06:48:41 +0000

    butterfly pattern

---
+++ 04_pattern/butterfly.java
@@ -0,0 +1,40 @@
+public class butterfly {
+    public static void pattern(int n){
+        // 1st half
+        // outter loop
+        for(int i=1; i<=n; i++){
+            // stars -i
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            // spaces -2*(n-i)
+            for(int j=1; j<=2*(n-i); j++){
+                System.out.print(" ");
+            }
+            // stars -i
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        // 2nd half inverted
+        for(int i=n; i>=1; i--){
+            // stars -i
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            // spaces -2*(n-i)
+            for(int j=1; j<=2*(n-i); j++){
+                System.out.print(" ");
+            }
+            // stars -i
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        pattern(10);
+    }
+}
