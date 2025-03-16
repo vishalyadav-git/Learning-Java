eb78915b3bae1040a672d3e372be4da020cc7bde
Vishal Yadav <yadavvishal00101@gmail.com>
2025-03-16 07:12:38 +0000

    2d array creation

---
+++ 07_2DAraay/creation.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class creation {
+    // public static boolean create(int matrix[][]){
+
+    // }
+   public static void main(String[] args) {
+    int matrix[][] = new int[3][3];
+    int n = 3; int m =3;
+    // input
+    Scanner sc = new Scanner(System.in);
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            matrix[i][j] = sc.nextInt();
+        }
+    }
+
+    // output
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            System.out.print(matrix[i][j] +" ");
+            }
+            System.out.println();
+        }
+   } 
+}
