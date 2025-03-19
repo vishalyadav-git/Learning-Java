858abce1bdecaeabeed0ab2be7a74712fdb7a993
Vishal Yadav <yadavvishal00101@gmail.com>
2025-03-19 14:30:51 +0000

    Diagonal Addition

---
+++ 07_2DAraay/diagonalMatrix.java
@@ -0,0 +1,25 @@
+public class diagonalMatrix {
+    // time complexity O(n^2)
+    public static void diagonal(int matrix[][]){
+        int sum = 0;
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
+                if(i==j){
+                    sum += matrix[i][j];
+                }
+                else if(i+j == matrix.length-1){
+                    sum += matrix[i][j];
+                }
+            }
+        }
+        System.out.println(sum);
+    }
+    public static void main(String[] args) {
+        int matrix[][] = {{1,2,3,4},
+                          {4,5,6,7},
+                          {8,9,10,11},
+                          {12,13,14,15},};
+         diagonal(matrix);                 
+
+    }
+}
