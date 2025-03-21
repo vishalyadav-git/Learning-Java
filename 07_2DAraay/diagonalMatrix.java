56bd4ba669d11229e119c0c1292a2008b1b5bb1b
Vishal Yadav <yadavvishal00101@gmail.com>
2025-03-21 15:18:05 +0000

    diagonal matrix sum

--- 07_2DAraay/diagonalMatrix.java
+++ 07_2DAraay/diagonalMatrix.java
@@ -1,18 +1,19 @@
 public class diagonalMatrix {
-    // time complexity O(n^2)
-    public static void diagonal(int matrix[][]){
+    public static void diagonal(int matrix[][]){ // time complexity O(n^2)
         int sum = 0;
-        for(int i=0; i<matrix.length; i++){
-            for(int j=0; j<matrix[0].length; j++){
-                if(i==j){
-                    sum += matrix[i][j];
-                }
-                else if(i+j == matrix.length-1){
-                    sum += matrix[i][j];
-                }
-            }
-        }
-        System.out.println(sum);
+        // for(int i=0; i<matrix.length; i++){
+        //     for(int j=0; j<matrix[0].length; j++){
+        //         if(i==j){
+        //             sum += matrix[i][j];
+        //         }
+        //         else if(i+j == matrix.length-1){
+        //             sum += matrix[i][j];
+        //         }
+        //     }
+        // }
+        // System.out.println(sum);
+
+        
     }
     public static void main(String[] args) {
         int matrix[][] = {{1,2,3,4},
