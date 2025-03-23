111d5224ced38021d7b6788504641305a677fa9d
Vishal Yadav <yadavvishal00101@gmail.com>
2025-03-23 07:57:28 +0000

    largest string using lexicografic concept

---
+++ 08_String/largestString.java
@@ -0,0 +1,14 @@
+public class largestString {
+    public static void main(String[] args) {
+        String fruits[] = {"apple", "kiwi", "mango", "strawberry"};
+        String largest = fruits[0];
+        for(int i=1; i<fruits.length; i++){
+            if(largest.compareToIgnoreCase(fruits[i]) < 0){
+                largest = fruits[i];
+            }
+        }
+        System.out.println(largest);
+
+
+    }
+}
