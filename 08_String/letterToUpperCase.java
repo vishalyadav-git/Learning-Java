825f7dc34833e3f6c73911224e631668698f69bc
Vishal Yadav <yadavvishal00101@gmail.com>
2025-03-24 07:31:13 +0000

    letterToUpperCase

---
+++ 08_String/letterToUpperCase.java
@@ -0,0 +1,24 @@
+public class letterToUpperCase {
+    public static String conversion(String str){
+        StringBuilder sb = new StringBuilder("");
+        char ch = Character.toUpperCase(str.charAt(0));
+        sb.append(ch);
+
+        for(int i=1; i<str.length(); i++){
+            if(str.charAt(i)== ' ' && i<str.length()-1){
+                sb.append(' ');
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));
+            }
+            else{
+                sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+        String str = "hi i am vishal yadav learning java";
+        System.out.println(conversion(str));
+    }
+    
+}
