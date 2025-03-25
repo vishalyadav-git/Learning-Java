743558e5ac73ec78b232d2b6fe14d639270eab1c
Vishal Yadav <yadavvishal00101@gmail.com>
2025-03-25 16:12:19 +0000

    oddEvenBIt

--- 08_String/oddEvenBIt.java
+++ 08_String/oddEvenBIt.java
@@ -1,6 +1,7 @@
-public class oddEvenBIt {
 
-        // Method to check if a number is even or odd using bit manipulation
+public class oddEvenBIt {
+    // Method to check if a number is even or odd using bit manipulation
+    
         public static void checkEvenOdd(int n) {
             if ((n & 1) == 0) {
                 System.out.println(n + " is Even");
@@ -16,7 +17,8 @@ public class oddEvenBIt {
             checkEvenOdd(0); // Even
             checkEvenOdd(15); // Odd
             checkEvenOdd(-8); // Even
-            checkEvenOdd(-11); // Odd
+            checkEvenOdd(-11); // Odd 
+            checkEvenOdd(+1); // Odd 
         }
     }
     
