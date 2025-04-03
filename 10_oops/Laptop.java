cf407f620b54635444d9c06432c50ad0e99ae80d
Vishal Yadav <yadavvishal00101@gmail.com>
2025-04-03 17:21:04 +0000

    Laptop example

---
+++ 10_oops/Laptop.java
@@ -0,0 +1,31 @@
+// Laptop class representing an object
+public class Laptop {
+    // Attributes (Instance Variables)
+    String brand;
+    String processor;
+    int price;
+
+    // Constructor
+    public Laptop(String brand, String processor, int price) {
+        this.brand = brand;
+        this.processor = processor;
+        this.price = price;
+    }
+
+    // Method to display laptop details
+    public void displayLaptopInfo() {
+        System.out.println("Brand: " + brand + ", Processor: " + processor + ", Price: ₹" + price);
+    }
+
+    // Main method to test the class
+    public static void main(String[] args) {
+        // Creating objects of Laptop class
+        Laptop laptop1 = new Laptop("Dell", "Intel i7", 75000);
+        Laptop laptop2 = new Laptop("HP", "Ryzen 5", 65000);
+
+        // Displaying details
+        laptop1.displayLaptopInfo();
+        laptop2.displayLaptopInfo();
+    }
+}
+
