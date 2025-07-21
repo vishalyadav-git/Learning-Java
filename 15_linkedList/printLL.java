031dc10783ae2721725c3b5a7e04576c80382c2d
Vishal Yadav <yadavvishal00101@gmail.com>
2025-07-21 16:07:34 +0000

    print a LL

---
+++ 15_linkedList/printLL.java
@@ -0,0 +1,40 @@
+class Node {
+    int data;
+    Node next;
+    
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+
+public class printLL {
+    public static Node head;
+
+    // Function to print the linked list
+    public static void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    // Main function
+    public static void main(String[] args) {
+        // Manually creating linked list: 1 -> 2 -> 3
+        Node first = new Node(1);
+        Node second = new Node(2);
+        Node third = new Node(3);
+
+        head = first;
+        first.next = second;
+        second.next = third;
+
+        printList();  // Output: 1 -> 2 -> 3 -> null
+    }
+}
+
+
