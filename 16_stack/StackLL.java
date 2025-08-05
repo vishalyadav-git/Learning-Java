ccfa5e15265422f5bd713dda923c03269e19ce1f
Vishal Yadav <yadavvishal00101@gmail.com>
2025-08-05 15:47:09 +0000

    stack using LL

---
+++ 16_stack/StackLL.java
@@ -0,0 +1,61 @@
+public class StackLL {
+        static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static class Stack {
+        static Node head = null;
+
+        // isEmpty
+        public static boolean isEmpty() {
+            return head == null;
+        }
+
+        // push
+        public static void push(int data) {
+            Node newNode = new Node(data);
+            newNode.next = head;
+            head = newNode;
+        }
+
+        // pop
+        public static int pop() {
+            if (isEmpty()) {
+                System.out.println("Stack is empty");
+                return -1;
+            }
+            int top = head.data;
+            head = head.next;
+            return top;
+        }
+
+        // peek
+        public static int peek() {
+            if (isEmpty()) {
+                System.out.println("Stack is empty");
+                return -1;
+            }
+            return head.data;
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack s = new Stack();
+
+        s.push(10);
+        s.push(20);
+        s.push(30);
+
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+
+}
