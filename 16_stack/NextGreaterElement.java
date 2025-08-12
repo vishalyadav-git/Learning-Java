d1173ae930e2455b4f8a0de4679be2c3d277e85a
Vishal Yadav <yadavvishal00101@gmail.com>
2025-08-12 14:50:46 +0000

    next greater element

--- 16_stack/NextGreaterElement.java
+++ 16_stack/NextGreaterElement.java
@@ -5,7 +5,7 @@ public class NextGreaterElement {
      public static void main(String[] args) {
         int arr[] = {4, 5, 2, 25};
         int n = arr.length;
-        int[] nge = new int[n];
+        int[] nextGreater = new int[n];
         Stack<Integer> stack = new Stack<>();
 
         // Traverse from right to left
@@ -15,14 +15,14 @@ public class NextGreaterElement {
                 stack.pop();
             }
 
-            // If stack is empty no NGE
-            nge[i] = stack.isEmpty() ? -1 : stack.peek();
+            // If stack is empty no nextGreater
+            nextGreater[i] = stack.isEmpty() ? -1 : stack.peek();
 
             // Push current element
             stack.push(arr[i]);
         }
 
         // Print result
-        System.out.println(Arrays.toString(nge));
+        System.out.println(Arrays.toString(nextGreater));
     }
 }
